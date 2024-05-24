337074d831d64450855f6a0f1c71df62c5947fee
yuta369 <y.m.36912@gmail.com>
2024-05-24 17:48:47 +0000

    [Add] いいね機能

--- rails6-bookers2-debug-ruby3-main/app/models/user.rb
+++ rails6-bookers2-debug-ruby3-main/app/models/user.rb
@@ -4,15 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :books, dependent: :destroy
   has_one_attached :profile_image
+  has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
-  
-  
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+
 end
