9b7cb9fd16f0e60224d30429120dd2734a01c95a
yuta369 <y.m.36912@gmail.com>
2024-05-26 07:06:44 +0000

    [Fix] 修正

--- rails6-bookers2-debug-ruby3-main/README.md
+++
@@ -1,3 +0,0 @@
-# DMM WEBCAMPコンテンツ【復習編】
-
-DMM WEBCAMPの学習コンテンツBookers2を完成させよう(デバッグ形式)の研修課題です
--- rails6-bookers2-debug-ruby3-main/app/models/post_comment.rb
+++
@@ -1,8 +0,0 @@
-class PostComment < ApplicationRecord
-  
-  belongs_to :user
-  belongs_to :book
-  
-  validates :comment, presence: true
-  
-end
