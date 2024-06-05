872feeb3a243891ee5206c0714ba7841c5a2ef27
yuta369 <y.m.36912@gmail.com>
2024-06-05 13:38:27 +0000

    [Add] 過去の投稿数の表示

--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -4,8 +4,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books
     @book = Book.new
+    @books = @user.books
+    @today_book =  @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
   end
 
   def index
